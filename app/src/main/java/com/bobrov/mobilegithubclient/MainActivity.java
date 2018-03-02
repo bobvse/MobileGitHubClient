a5a57dbb69a262ee1006d399fdc985269ea76ada
Vladimir Bobrov <bobvse@gmail.com>
2018-03-02 11:03:34 +0300

    Добавление разметки главного экрана

--- app/src/main/java/com/bobrov/mobilegithubclient/MainActivity.java
+++ app/src/main/java/com/bobrov/mobilegithubclient/MainActivity.java
@@ -1,13 +1,40 @@
 package com.bobrov.mobilegithubclient;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView twLogin;
+    private TextView twPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        twLogin = findViewById(R.id.main_activity_login_editText);
+        twPassword = findViewById(R.id.main_activity_password_editText);
+
+        findViewById(R.id.main_activity_login_button).setOnClickListener(this);
+        findViewById(R.id.main_activity_logout_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.main_activity_login_button:
+                Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.main_activity_logout_button:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
